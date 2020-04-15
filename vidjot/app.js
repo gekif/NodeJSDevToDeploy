21b96ea850f24420f813ad4f06e59d2815e87385
Gekif <dzulfikar.maulana@gmail.com>
2020-04-15 20:20:32 +0700

    03-vidjot-project-start-express-fundamentals: 012-express-middleware

--- vidjot/app.js
+++ vidjot/app.js
@@ -3,9 +3,22 @@ const express = require('express');
 const app = express();
 
 
+// How Middleware Works
+app.use((req, res, next) => {
+    // console.log(Date.now());
+
+    req.name = 'Dzulfikar Maulana';
+
+    next();
+});
+
+
 // Index Route
 app.get('/', (req, res) => {
-    res.send('INDEX');
+    console.log(req.name);
+
+    // res.send('INDEX');
+    res.send(req.name);
 });
 
 // About Route
