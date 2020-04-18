8ce80904955cc1a52ecd3c43d5628355a494296f
Gekif <dzulfikar.maulana@gmail.com>
2020-04-18 21:25:29 +0700

    04-mongoose-local-mongodb: 023-remove-idea-data

--- vidjot/app.js
+++ vidjot/app.js
@@ -137,6 +137,14 @@ app.put('/ideas/:id', (req, res) => {
         })
 });
 
+// Delete Idea
+app.delete('/ideas/:id', (req, res) => {
+    Idea.remove({ _id: req.params.id })
+        .then(() => {
+            res.redirect('/ideas');
+        });
+});
+
 
 const port = 5000;
 
