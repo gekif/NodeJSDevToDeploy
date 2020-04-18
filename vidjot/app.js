5c3fbe55cc4bd7f56b55b249d743c122d774c05d
Gekif <dzulfikar.maulana@gmail.com>
2020-04-18 19:34:24 +0700

    04-mongoose-local-mongodb: 017-add-idea-form

--- vidjot/app.js
+++ vidjot/app.js
@@ -45,6 +45,11 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add');
+});
+
 
 const port = 5000;
 
