384253a9e6b7a02dc51c4b01e74dd4e05a795b1a
Gekif <dzulfikar.maulana@gmail.com>
2020-04-19 11:06:32 +0700

    05-local-authentication-with-passport: 026-the-express-router

--- vidjot/app.js
+++ vidjot/app.js
@@ -9,6 +9,11 @@ const mongoose = require('mongoose');
 const app = express();
 
 
+// Load routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+
 // Map Global Promise - Get Rid of Warning
 mongoose.Promise = global.Promise;
 
@@ -19,13 +24,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/vidjot-dev', {
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err)
 );
-
-
-// Load Idea Model
-require('./models/Idea');
-
-const Idea = mongoose.model('ideas');
-
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -77,109 +75,9 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// Idea Index Page
-app.get('/ideas', (req, res) => {
-    Idea.find({})
-        .sort({ date: 'desc' })
-        .then(ideas => {
-            res.render('ideas/index', {
-                ideas: ideas
-            });
-        });
-});
-
-// Add Idea Form
-app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add');
-});
-
-// Edit Idea Form
-app.get('/ideas/edit/:id', (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    })
-        .then(idea => {
-            res.render('ideas/edit', {
-                idea: idea
-            });
-        });
-});
-
-// Add Form Process
-app.post('/ideas', (req, res) => {
-    let errors = [];
-
-    if (!req.body.title) {
-        errors.push({ text: 'Please add a title' });
-    }
-
-    if (!req.body.details) {
-        errors.push({ text: 'Please add a some details' });
-    }
-
-    if (errors.length > 0) {
-        res.render('ideas/add', {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        });
-
-    } else {
-        const newUser = {
-            title: req.body.title,
-            details: req.body.details
-        };
-
-        new Idea(newUser)
-            .save()
-            .then(idea => {
-                req.flash('success_msg', 'Video idea added');
-
-                res.redirect('/ideas');
-
-                console.log('Insert Success');
-
-                console.log(idea._doc);
-            });
-    }
-});
-
-// Edit Form Process
-app.put('/ideas/:id', (req, res) => {
-    Idea.findOne({
-        _id: req.params.id,
-    })
-        .then(idea => {
-            // New Values
-            idea.title = req.body.title;
-            idea.details = req.body.details;
-
-            idea.save()
-                .then(idea => {
-                    req.flash('success_msg', 'Video idea updated');
-
-                    res.redirect('/ideas');
-
-                    console.log('Updated Success');
-
-                    console.log(idea._doc);
-                });
-        })
-});
-
-// Delete Idea
-app.delete('/ideas/:id', (req, res) => {
-    Idea.remove({ _id: req.params.id })
-        .then(() => {
-            req.flash('success_msg', 'Video idea removed');
-
-            res.redirect('/ideas');
-
-            console.log('Deleted Success');
-
-            console.log(idea._doc);
-        });
-});
+// Use routes
+app.use('/ideas', ideas);
+app.use('/users', users);
 
 
 const port = 5000;
---
+++ vidjot/routes/users.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+
+
+// User Login Route
+router.get('/login', (req, res) => {
+
+});
+
+// User Register Route
+router.get('/register', (req, res) => {
+
+});
+
+
+module.exports = router;
